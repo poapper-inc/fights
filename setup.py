0a26d5b2132149929c6fc49cbed161a50697a511
Ryang Sohn <loop.infinitely@gmail.com>
2023-02-17 16:25:17 +0900

    Fix a typo

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ import numpy as np
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
-figths_envs_path = join("fights", "envs")
+fights_envs_path = join("fights", "envs")
 defs = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
 puoribor = Extension(
     "puoribor_cython",
-    sources=[join(figths_envs_path, "puoribor_cython.pyx")],
+    sources=[join(fights_envs_path, "puoribor_cython.pyx")],
     include_dirs=[np.get_include()],
     define_macros=defs,
 )
 quoridor = Extension(
     "quoridor_cython",
-    sources=[join(figths_envs_path, "quoridor_cython.pyx")],
+    sources=[join(fights_envs_path, "quoridor_cython.pyx")],
     include_dirs=[np.get_include()],
     define_macros=defs,
 )
