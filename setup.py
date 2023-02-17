ef8d516a82db835e8698cf2dcbfbf32a6fbf2845
Ryang Sohn <loop.infinitely@gmail.com>
2023-02-17 16:14:10 +0900

    Add setup.py for cython build

---
+++ setup.py
@@ -0,0 +1,22 @@
+from os.path import join
+
+import numpy as np
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+figths_envs_path = join("fights", "envs")
+defs = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
+puoribor = Extension(
+    "puoribor_cython",
+    sources=[join(figths_envs_path, "puoribor_cython.pyx")],
+    include_dirs=[np.get_include()],
+    define_macros=defs,
+)
+quoridor = Extension(
+    "quoridor_cython",
+    sources=[join(figths_envs_path, "quoridor_cython.pyx")],
+    include_dirs=[np.get_include()],
+    define_macros=defs,
+)
+
+setup(ext_modules=cythonize([puoribor, quoridor]))
