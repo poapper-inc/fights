561a05934fd0a564911f994c397c3b160891aa99
Ryang Sohn <loop.infinitely@gmail.com>
2023-02-21 15:39:46 +0900

    Fix cython source directories

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import numpy as np
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
-fights_envs_path = join("fights", "envs")
+fights_envs_path = join("src", "fights", "envs")
 defs = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
 puoribor = Extension(
-    "puoribor_cython",
+    "fights.envs.puoribor_cython",
     sources=[join(fights_envs_path, "puoribor_cython.pyx")],
     include_dirs=[np.get_include()],
     define_macros=defs,
 )
 quoridor = Extension(
-    "quoridor_cython",
+    "fights.envs.quoridor_cython",
     sources=[join(fights_envs_path, "quoridor_cython.pyx")],
     include_dirs=[np.get_include()],
     define_macros=defs,
